a6e0191e767f25e6cba0316ea3a8b5c8d2ca55f7
ridait <rejra2012@gmail.com>
2019-09-24 01:39:56 +0100

    implement middleware to secure /secrets and /users endpoints with access token

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ const secrets = [
   }
 ];
 
-app.get("/users", (req, res) => {
-  res.send(users);
+app.get("/users", authenticateToken, (req, res) => {
+  const body = { connectedAs: req.user, users };
+  res.status(200).send(body);
 });
 
 app.post("/login", async (req, res) => {
@@ -61,11 +62,33 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.get("/secrets", (req, res) => {
-  res.json(secrets);
+app.get("/secrets", authenticateToken, (req, res) => {
+  const body = { connectedAs: req.user, secrets };
+  res.status(200).send(body);
 });
 
 app.listen(3000);
+
+function authenticateToken(req, res, next) {
+  const accessToken = extractAccessToken(req);
+  if (accessToken == null) {
+    res.status(401).send();
+  } else {
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+      if (error) res.status(403).send();
+      else {
+        req.user = user;
+        next();
+      }
+    });
+  }
+}
+
+const extractAccessToken = req => {
+  const authHeader = req.headers["authorization"];
+  const accessToken = authHeader && authHeader.split(" ")[1];
+  return accessToken;
+};
 
 const extractLoginRequest = req => {
   return ({ username, password } = req.body);
